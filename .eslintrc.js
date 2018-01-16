205becc6e65dcea8e4222bb8f737a8dc8ed2ea05
wangqun6 <wangqun28>
2018-01-16 21:20:58 +0800

    change

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
         "require": true
     },
     "parserOptions": {
-        "ecmaVersion": 6,
+        "ecmaVersion": 8,
         "sourceType": "script"
     },
     "rules": {
