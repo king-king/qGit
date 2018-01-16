0a19f526f165776819d10eaa326dd0d7a80decb3
wangqun6 <wangqun28>
2018-01-16 22:01:10 +0800

    add bootrap

--- main.js
+++ main.js
@@ -12,9 +12,7 @@ let win;
 function createWindow() {
     // 创建浏览器窗口。
     win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        frame: false
+        autoHideMenuBar: true
     });
 
     // 然后加载应用的 index.html。
